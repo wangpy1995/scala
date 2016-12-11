711a2634457aae1cbbd1e1dd26c732ab7ac3d760
wpy <592114387@qq.com>
2016-12-11 23:46:13 +0800

    correct some compile error

--- spark2.11_mvn/src/main/scala/TestSparkWordCount.scala
+++ spark2.11_mvn/src/main/scala/TestSparkWordCount.scala
@@ -1,4 +1,6 @@
+import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.util.TimeStampedValue
 
 /**
   * Created by w5921 on 2016/11/18.
@@ -9,4 +11,25 @@ object TestSparkWordCount {
     val ss = SparkSession.builder().appName("sparkWordCount").master("local").getOrCreate()
     ss.sparkContext.textFile("./demo/words.log").map((_, 1)).reduceByKey(_ + _).collect().foreach(println)
   }
+
+  def testHbase(): Unit = {
+    val writeCatalog = " "
+    val tsSpecified = TimeStampedValue
+    val conf = new SparkConf().setAppName("testHbase").setMaster("local[")
+    val sqlContext = SparkSession.builder().config(conf).getOrCreate().sqlContext
+    val df = sqlContext.read
+      .options(Map(HBaseTableCatalog.tableCatalog -> writeCatalog, HBaseSparkConf.TIMESTAMP -> tsSpecified.toString))
+      .format("org.apache.hadoop.hbase.spark")
+      .load()
+    df.registerTempTable("table")
+    sqlContext.sql("select count(*) from table").show
+  }
+}
+
+object HBaseTableCatalog {
+  val tableCatalog = ""
+}
+
+object HBaseSparkConf {
+  val TIMESTAMP = ""
 }
