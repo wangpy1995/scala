bf7081245a552e6fbdeb669681085e03c3bf2dc2
wpy <592114387@qq.com>
2017-06-15 17:03:51 +0800

    Some Codes : Async File Chanel

--- location/src/test/scala/org/apache/spark/test/TestRDD.scala
+++ location/src/test/scala/org/apache/spark/test/TestRDD.scala
@@ -3,6 +3,8 @@ package org.apache.spark.test
 import java.io.{File, IOException}
 import java.nio.ByteBuffer
 import java.nio.channels.{AsynchronousFileChannel, CompletionHandler}
+import java.nio.file.StandardOpenOption
+import java.util.concurrent.CountDownLatch
 
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{Partition, SparkConf, SparkContext, TaskContext}
@@ -19,6 +21,7 @@ object TestRDD {
   val sc = SparkContext.getOrCreate(sparkConf)
   val path = "/home/wpy/tmp/test/"
   val out = "/home/wpy/tmp/out"
+  val CAPACITY = 8 * 1000 * 1000
 
   def main(args: Array[String]): Unit = {
     val files = new File(path).listFiles()
@@ -47,49 +50,67 @@ class MyRDD[T: ClassTag](@transient sc: SparkContext, files: Array[File]) extend
     for (i <- partitionedData.indices) {
       partitionedData(i) = ArrayBuffer.empty[(Int, String)]
     }
+    val countDownLatch = new CountDownLatch(10)
     //非阻塞IO(回调方式)
     files.foreach { file =>
       //TODO 是否需要线程池
-      val reader = AsynchronousFileChannel.open(file.toPath)
+      val reader = AsynchronousFileChannel.open(file.toPath, StandardOpenOption.READ)
       //      此处若使用堆外内存会导致数据无法直接获取
-      val dst = ByteBuffer.allocate(file.length().toInt)
-      val completion = new CompletionHandler[Integer, Array[ArrayBuffer[(Int, String)]]] {
-        override def failed(exc: Throwable, attachment: Array[ArrayBuffer[(Int, String)]]): Unit = {
+      var pos = 0L
+      val len = file.length()
+      val dst = ByteBuffer.allocate(len.toInt)
+      val handler = new CompletionHandler[Integer, Array[ArrayBuffer[(Int, String)]]] {
+        override def failed(exc: Throwable, attachment: Array[ArrayBuffer[(Int, String)]]): Unit =
           exc.printStackTrace()
-        }
 
         override def completed(result: Integer, attachment: Array[ArrayBuffer[(Int, String)]]): Unit = {
           if (result > 0) {
             dst.flip()
-            try {
-              val lines = new String(dst.array()).split("\n")
-              lines.foreach { line =>
-                val elements = line.split(" ", 2)
+            val lines = new String(dst.array()).split("\n")
+            lines.foreach { line =>
+              val elements = line.split(" ", 2)
+              try {
                 val key = elements.head.toInt
-                attachment(key / 10) += ((key, elements(1)))
+                attachment(key / 1000000) += ((key, elements.last))
+              } catch {
+                case e: IOException => e.printStackTrace()
+                case _: NumberFormatException => log.error(elements.head)
+                case _: ArrayIndexOutOfBoundsException => log.error((elements.head.toInt / 1000000).toString)
               }
-            } catch {
-              case e: IOException => e.printStackTrace()
             }
+            countDownLatch.countDown()
             dst.clear()
+            //            dst.asInstanceOf[DirectBuffer].cleaner().clean()
           } else reader.close()
+          log.info(s"pos: $pos")
         }
       }
-      reader.read(dst, 0L, partitionedData, completion)
+      //      for (_ <- 0 to len.toInt / TestRDD.CAPACITY) {
+      reader.read(dst, 0, partitionedData, handler)
+      //        pos += TestRDD.CAPACITY
+      //      }
       //TODO do other things if needed
     }
 
-    /* files.foreach { file =>
-       val reader = new BufferedReader(new FileReader(file))
-       var line = reader.readLine()
-       while (line != null) {
-         val elements = line.split(" ", 2)
-         val key = elements.head.toInt
-         partitionedData(key / 10) += ((key, elements(1)))
-         line = reader.readLine()
-       }
-       reader.close()
-     }*/
+    /*files.foreach { file =>
+      val reader = new BufferedReader(new FileReader(file))
+      var line = reader.readLine()
+      while (line != null) {
+        val elements = line.split(" ", 2)
+        try {
+          val key = elements.head.toInt
+          partitionedData(key / 1000000) += ((key, elements.last))
+        } catch {
+          case e: IOException => e.printStackTrace()
+          case _: NumberFormatException => log.error(elements.head)
+          case _: ArrayIndexOutOfBoundsException => log.error((elements.head.toInt / 1000000).toString)
+        }
+        line = reader.readLine()
+      }
+      reader.close()
+    }*/
+
+    countDownLatch.await()
     partitionedData.filter(_.nonEmpty).zipWithIndex
       .map(p => new MyPartition[(Int, String)](p._2, p._1))
   }
